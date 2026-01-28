b74e8c37e38a55165b4d2a984f8f549dae5762fe
praveen <238w1a1285@vrsec.ac.in>
2026-01-28 19:47:37 +0530

    setting up local and production

--- src/components/MapWithTimeseries.jsx
+++ src/components/MapWithTimeseries.jsx
@@ -37,7 +37,7 @@ ChartJS.register(
   Tooltip
 );
 
-const BACKEND_URL = "http://127.0.0.1:5000";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 // Click handler enabling point picking + smooth animation
 function ClickHandler({ onClick }) {
