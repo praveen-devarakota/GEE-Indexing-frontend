ab218e9b1b47147bbf1d4b307a893461f056ef56
Panduranga <panduranga1797@gmail.com>
2026-01-28 11:05:06 +0530

    chnages

--- src/components/MapWithTimeseries.jsx
+++ src/components/MapWithTimeseries.jsx
@@ -37,7 +37,7 @@ ChartJS.register(
   Tooltip
 );
 
-const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = "http://127.0.0.1:5000";
 
 // Click handler enabling point picking + smooth animation
 function ClickHandler({ onClick }) {
